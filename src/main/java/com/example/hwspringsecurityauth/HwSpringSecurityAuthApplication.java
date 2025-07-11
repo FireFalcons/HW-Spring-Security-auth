361fd3bd5af468b09ae57a535503da5a2cbe8f93
Andriy <kozakua51@gmail.com>
2025-07-11 12:06:54 +0300

    Initial commit

---
+++ src/main/java/com/example/hwspringsecurityauth/HwSpringSecurityAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwspringsecurityauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringSecurityAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSpringSecurityAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hwspringsecurityauth/HwSpringSecurityAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwspringsecurityauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringSecurityAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
